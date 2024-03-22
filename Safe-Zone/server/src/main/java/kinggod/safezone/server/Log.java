9e3739a4d9b3735e0a7dd832f6173edda30be5d1
AlmondGood <jwt2358@naver.com>
2024-03-22 20:52:54 +0900

    fix: LocalDateTime 포맷을 못 받는 문제 수정

--- Safe-Zone/server/src/main/java/kinggod/safezone/server/Log.java
+++ Safe-Zone/server/src/main/java/kinggod/safezone/server/Log.java
@@ -2,6 +2,7 @@ package kinggod.safezone.server;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -12,18 +13,22 @@ import java.util.UUID;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@ToString
 public class Log {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private UUID uuid;
     private String location;
+    // 도움 요청 발생 시각
     private LocalDateTime occurrenceTime;
+    // 경찰 신고 시각
     private LocalDateTime reportTime;
 
     public Log(LogRequest logRequest) {
         this.uuid = logRequest.getUuid();
         this.location = logRequest.getLocation();
         this.occurrenceTime = logRequest.getOccurrenceTime();
+        this.reportTime = LocalDateTime.now();
     }
 }
--- Safe-Zone/server/src/main/java/kinggod/safezone/server/LogRequest.java
+++ Safe-Zone/server/src/main/java/kinggod/safezone/server/LogRequest.java
@@ -1,16 +1,19 @@
 package kinggod.safezone.server;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@ToString
 public class LogRequest {
 
     private UUID uuid;
     private String location;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime occurrenceTime;
 }
