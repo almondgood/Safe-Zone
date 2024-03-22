350b7bc9343436e382be68c89616add2fe1ba69c
AlmondGood <jwt2358@naver.com>
2024-03-22 20:52:15 +0900

    feat: 로그 생성, 조회 및 신고 기능 추가
    - 로그는 uuid로 조회

--- Safe-Zone/server/src/main/java/kinggod/safezone/server/LogController.java
+++ Safe-Zone/server/src/main/java/kinggod/safezone/server/LogController.java
@@ -1,27 +1,66 @@
 package kinggod.safezone.server;
 
 import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping(name="/api")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class LogController {
 
     private final LogService logService;
+    private final DefaultMessageService messageService;
 
-    @GetMapping(name = "/logs")
+    @GetMapping("/logs")
     public List<Log> getAllLogs() {
         return logService.getAllLogs();
     }
 
-    @PostMapping(name = "data")
-    public void createLog(@RequestBody Log log) {
-        // 이 부분에서 JSON 데이터를 Log 객체로 파싱하여 Service에 전달합니다.
-        logService.createLog(log);
-
+    @GetMapping("/log/{uuid}")
+    public Log getLogByUuid(@RequestParam UUID uuid) {
+        return logService.getLogByUuid(uuid);
     }
 
+    @PostMapping(value="/data", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<LogRequest> createLog(@RequestBody LogRequest logRequest) {
+        reportToPolice(logRequest);
+        try {
+            logService.createLog(logRequest);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(logRequest, HttpStatus.OK);
+    }
+
+    // 경찰 신고
+    private SingleMessageSentResponse reportToPolice(LogRequest logRequest) {
+        // 실제 신고 대신 임의의 번호로 문자 전송
+        Message msg = new Message();
+        StringBuilder sb = new StringBuilder();
+
+        msg.setFrom("01033842719");
+        msg.setTo("01023578981");
+        msg.setText("도와주세용");
+
+        // 실제 사용 시에는 주석 해제
+        // 사용할때 돈 듦;
+        SingleMessageSentResponse response = null;
+        //SingleMessageSentResponse response =
+        //        this.messageService.sendOne(new SingleMessageSendingRequest(msg));
+
+
+        return response;
+    }
 }
--- Safe-Zone/server/src/main/java/kinggod/safezone/server/LogService.java
+++ Safe-Zone/server/src/main/java/kinggod/safezone/server/LogService.java
@@ -1,48 +1,56 @@
 package kinggod.safezone.server;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class LogService {
 
     private final LogRepository logRepository;
-
-    public void parse(String body) {
-        
-    }
+    private static final Logger logger = LoggerFactory.getLogger(LogService.class);
 
     public List<Log> getAllLogs() {
         return logRepository.findAll();
     }
 
 
-    public void createLog(Log log) throws IllegalArgumentException {
-        validateLog(log);
+    public void createLog(LogRequest logRequest) throws IllegalArgumentException {
+        validateLog(logRequest);
+        Log log = new Log(logRequest);
         logRepository.save(log);
+
+        logger.info(log.toString());
     }
 
-    private void validateLog(Log log) throws IllegalArgumentException {
-        if (log == null) {
+    private void validateLog(LogRequest logRequest) throws IllegalArgumentException {
+        if (logRequest == null) {
             throw new IllegalArgumentException("Log cannot be null");
         }
-        if (log.getUuid() == null) {
+        if (logRequest.getUuid() == null) {
             throw new IllegalArgumentException("UUID cannot be null");
         }
-        if (log.getLocation() == null || log.getLocation().isEmpty()) {
+        if (logRequest.getLocation() == null || logRequest.getLocation().isEmpty()) {
             throw new IllegalArgumentException("Location cannot be null or empty");
         }
-        if (log.getOccurrenceTime() == null || log.getReportTime() == null ||
-                log.getOccurrenceTime().isAfter(LocalDateTime.now()) ||
-                log.getReportTime().isAfter(LocalDateTime.now())) {
-            throw new IllegalArgumentException("Occurrence time and report time must be in the past");
+        if (logRequest.getOccurrenceTime() == null ||
+                logRequest.getOccurrenceTime().isAfter(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Occurrence time must be in the past");
         }
+
+    }
+
+    public Log getLogByUuid(UUID uuid) {
+        return logRepository.findByUuid(uuid).orElseThrow(EntityNotFoundException::new);
     }
 }
 
